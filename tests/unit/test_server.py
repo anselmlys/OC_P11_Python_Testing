2455a071254963cba49bc2cd9a5921099aaa8fa7
anselmlys <anselm.lisa@gmail.com>
2026-02-23 12:08:27 +0100

    fix club's total points are not updated after a booking

--- tests/unit/test_server.py
+++ tests/unit/test_server.py
@@ -32,7 +32,7 @@ def test_show_summary_valid_email(client, monkeypatch):
 
     assert response.status_code == 200
     assert b'test@club.com' in response.data
-    assert b'Test Competition' in response.data
+    assert b'Points available:' in response.data
 
 
 def test_show_summary_invalid_email(client, monkeypatch):
@@ -51,3 +51,31 @@ def test_show_summary_invalid_email(client, monkeypatch):
     assert response.status_code == 200
     assert 'Please enter your secretary email to continue:' in page
     assert 'Sorry, that email wasn\'t found.' in page
+
+
+def test_purchase_places_with_enough_points(client, monkeypatch):
+    monkeypatch.setattr(server, 'competitions', [
+        {'name': 'Test Competition', 'date': '2026-02-12 12:30:00', 'numberOfPlaces': '15'}
+    ])
+    monkeypatch.setattr(server, 'clubs', [
+        {'name': 'Test Club', 'email': 'test@club.com', 'points': '10'}
+    ])
+
+    response = client.post(
+        '/purchasePlaces',
+        data={
+            'competition': 'Test Competition',
+            'club': 'Test Club',
+            'places': '5',
+        }
+    )
+
+    assert response.status_code == 200
+    # Verify if flash message appear
+    assert b'Great-booking complete!' in response.data
+    # Verify an element of the welcome template to check if it is displayed properly
+    assert b'Points available:' in response.data
+    # Check that the available competition places have been updated
+    assert server.competitions[0]['numberOfPlaces'] == 10
+    # Check that the points used have been deducted from the club's total
+    assert server.clubs[0]['points'] == 5
